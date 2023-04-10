50d7b368e8af02c37db3c31e4ab450df5e4559cd
CrisAcosta1705 <128009007+CrisAcosta1705@users.noreply.github.com>
2023-04-09 22:50:37 -0600

    Add files via upload

---
+++ FuncionY.java
@@ -0,0 +1,12 @@
+package Calculo;
+
+public class FuncionY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double X = 1;
+		double Y = 5 * Math.pow(X, 4) + 2 * Math.pow(X, 3) + 3 * Math.pow(X, 2) + 7;
+		System.out.println("El valor de Y es: " + Y);
+	}
+
+}
